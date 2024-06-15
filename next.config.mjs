fadaadfc0f1d18f8b1ddb3d169ecef17226b1ee5
Asharib Ali <asharibarain4@gmail.com>
2024-06-15 12:25:51 +0500

    fix the issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+const nextConfig = {};
 
-  // Add basePath
-  basePath: "/github-pages",
-};
-
-module.exports = nextConfig;
+export default nextConfig;
